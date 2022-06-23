354045ed41fd3b4f29802aca3cab125989125648
lazarus <projatochakraborty@gmail.com>
2022-06-23 21:35:15 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get minimal.</p>
+    </>
+  );
+};
+
+export default App;
